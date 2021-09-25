97446651360744025e23f8adc22c275c55983c61
theo902b <84502954+theo902b@users.noreply.github.com>
2021-09-25 23:35:40 +0200

    flemmard intelligent = php include

---
+++ html_css/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div></div>
+    <a href ="conditions_et_regles.html">○ Conditions générales et règles de communauté</a>
+    <a href ="charte_cookies.html">○ Charte d'utilisation des cookies</a>
+    <a href ="conseil_secu.html">○ Conseil de sécurité</a>
+    <a href ="politique_confidentialite.html">○ Politique de confidentialité</a>
+    <div></div>
+</footer>
+
---
+++ html_css/header.php
@@ -0,0 +1,16 @@
+
+    <header>
+    <img id="banniere" src="img/banniere.png">
+    <nav>
+        <div></div>
+        <img class ="boxnav" id ="imgnavexclu" src="img/logo.png"><img>
+        <a class ="boxnav boxnav2" href="decouvrir.html">Decouvrir
+        <a class ="boxnav boxnav2" href="recherche.html">Recherche</a>
+        <a class ="boxnav boxnav2" href="visite.html">Visites</a>
+        <a class ="boxnav boxnav2"href="preferences.html">préférences</a>
+        <a class ="boxnav boxnav2" href="messagerie.html">Messagerie</a>
+        <a id ="anavexclu"></a>
+        <a class ="boxnav boxnav2" href="profil.html">Mon profil</a>
+        <div></div>
+    </nav>
+    </header> 
