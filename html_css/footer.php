446bc6e5a67a07f1c7c51c0b17321c64c9a2ba78
theo902b <84502954+theo902b@users.noreply.github.com>
2021-09-25 23:46:26 +0200

    Confirmation de l'aspect pratique d'un include haha ! changement des etentions des liens de mn header et footer

--- html_css/footer.php
+++ html_css/footer.php
@@ -1,9 +1,9 @@
 <footer>
     <div></div>
-    <a href ="conditions_et_regles.html">○ Conditions générales et règles de communauté</a>
-    <a href ="charte_cookies.html">○ Charte d'utilisation des cookies</a>
-    <a href ="conseil_secu.html">○ Conseil de sécurité</a>
-    <a href ="politique_confidentialite.html">○ Politique de confidentialité</a>
+    <a href ="conditions_et_regles.php">○ Conditions générales et règles de communauté</a>
+    <a href ="charte_cookies.php">○ Charte d'utilisation des cookies</a>
+    <a href ="conseil_secu.php">○ Conseil de sécurité</a>
+    <a href ="politique_confidentialite.php">○ Politique de confidentialité</a>
     <div></div>
 </footer>
 
