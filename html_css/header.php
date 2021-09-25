8dd28028e9d0965196096f16206e77e8cec71357
theo902b <84502954+theo902b@users.noreply.github.com>
2021-09-25 23:43:08 +0200

    Petit oublie = changement extentions dans les liens de mon header

--- html_css/header.php
+++ html_css/header.php
@@ -4,13 +4,13 @@
     <nav>
         <div></div>
         <img class ="boxnav" id ="imgnavexclu" src="img/logo.png"><img>
-        <a class ="boxnav boxnav2" href="decouvrir.html">Decouvrir
-        <a class ="boxnav boxnav2" href="recherche.html">Recherche</a>
-        <a class ="boxnav boxnav2" href="visite.html">Visites</a>
-        <a class ="boxnav boxnav2"href="preferences.html">préférences</a>
-        <a class ="boxnav boxnav2" href="messagerie.html">Messagerie</a>
+        <a class ="boxnav boxnav2" href="decouvrir.php">Decouvrir
+        <a class ="boxnav boxnav2" href="recherche.php">Recherche</a>
+        <a class ="boxnav boxnav2" href="visite.php">Visites</a>
+        <a class ="boxnav boxnav2"href="preferences.php">préférences</a>
+        <a class ="boxnav boxnav2" href="messagerie.php">Messagerie</a>
         <a id ="anavexclu"></a>
-        <a class ="boxnav boxnav2" href="profil.html">Mon profil</a>
+        <a class ="boxnav boxnav2" href="profil.php">Mon profil</a>
         <div></div>
     </nav>
     </header> 
