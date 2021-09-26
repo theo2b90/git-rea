288b6bef41c3b68ab58c7519c4ed3aac1d75c3b0
theo902b <84502954+theo902b@users.noreply.github.com>
2021-09-27 00:34:41 +0200

    Javascript MAJ généralisation par une fonction. Toujours bloqué

--- html_css/script.js
+++ html_css/script.js
@@ -1,14 +1,28 @@
+// var button1 = document.getElementById("button1");
+// var displayJS = document.getElementById("displayJS");
+
+// function buttonActionOnOff(){
+//     if(getComputedStyle(displayJS).display != "none"){
+//         displayJS.style.display="none";
+//     }
+//     else{
+//         displayJS.style.display="block";
+//     }
+// };
+// button1.onclick = buttonActionOnOff;
+
+// La fonction ne marche pas, Faire recherches. BUT : Faire Apparaitre et disparaitre le formulaire au clique du boutton. https://www.pierre-giraud.com/afficher-cacher-div-javascript/
+
 var button1 = document.getElementById("button1");
 var displayJS = document.getElementById("displayJS");
 
-button1.addEventListener("click", () => {
+function buttonActionOnOff(){
     if(getComputedStyle(displayJS).display != "none"){
-        displayJS.style.display="none";
+        displayJS.style.display = "none";
     }
     else{
-        displayJS.style.display="block";
+        displayJS.style.display = "block";
     }
-})
+};
 
-// La fonction ne marche pas, Faire recherches BUT : Faire Apparaitre et disparaitre le formulaire au clique du boutton. https://www.pierre-giraud.com/afficher-cacher-div-javascript/
-
+button1.onclick = buttonActionOnOff;
